d24a761f4a6c76215f5806c80984a8a1977c4423
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-03-21 00:35:05 +0100

    build: replace tsup with vite build, update config
    export proper files and type definition

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'index.ts'),
+      name: 'React-hooks',
+      fileName: (format) => `index.${format}.js`
+    },
+    rollupOptions: {
+      external: ['react'],
+      output: {
+        globals: {
+          react: 'React'
+        }
+      }
+    },
+    sourcemap: true,
+    emptyOutDir: true
+  }
 })
